8e4a2bc2a943774fc78feb5c7926fc9a330efa62
dohlee <dohlee.bioinfo@gmail.com>
2023-03-03 09:23:00 +0900

    0.0.5.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'spliceai-pytorch',
   packages = find_packages(exclude=[]),
   include_package_data = True,
-  version = '0.0.4',
+  version = '0.0.5',
   license='MIT',
   description = 'SpliceAI - Pytorch',
   author = 'Dohoon Lee',
